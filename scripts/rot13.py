b0ab3c77f42d8eb82e730b8ebdd73f23cffae519
Nkumar-15 <52885610+Nkumar-15@users.noreply.github.com>
2020-02-05 22:22:25 +0530

    Add files via upload

---
+++ scripts/rot13.py
@@ -0,0 +1,28 @@
+print("**********WELCOME TO ROT13**********")
+alphabet="abcdefghijklmnopqrstuvwxyz"
+#al=list(alphabet)
+out=''
+
+shift=13
+print("Shift=",shift)
+empty=[]
+char=input("enter the character=")
+print("The entered character =",char)
+char=char.lower()
+for i in char:
+    #print(i)
+    if i  not in alphabet:
+        print("hey man u should enter the character ya")
+        break
+    else:
+         n=alphabet.find(i)
+    n_shift=n+shift
+    if n_shift>=25 :
+        n_shift=n_shift-26
+    #print(n_shift)
+    outchar=alphabet[n_shift]
+    empty.append(outchar)
+    out="".join([str(j) for j in empty])
+   
+ 
+print(out)
