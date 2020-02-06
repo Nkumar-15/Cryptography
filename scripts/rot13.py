e81a32203afdc4365750190f3412d58be39d81b7
Nkumar-15 <52885610+Nkumar-15@users.noreply.github.com>
2020-02-06 15:47:10 +0530

    Update rot13.py

--- scripts/rot13.py
+++ scripts/rot13.py
@@ -1,3 +1,4 @@
+import re
 print("**********WELCOME TO ROT13**********")
 alphabet="abcdefghijklmnopqrstuvwxyz"
 #al=list(alphabet)
@@ -6,25 +7,45 @@ out=''
 shift=13
 print("Shift=",shift)
 empty=[]
-char=input("enter the character=")
+print("*Select your function: ")
+print("\t1.ROT13_Encode\n\t2.ROT13_Decode")
+m=int(input("Enter your option="))
+
+char=input("Enter the character=")
 print("The entered character =",char)
 char=char.lower()
-for i in char:
-    #print(i)
-    if i  not in alphabet:
-        print("hey man u should enter the character ya")
-        break
+
+if bool(re.search(r'\d',char)) :
+    print("\nHey man u should enter the character ya :(")
+    exit(1)
+    
+else:
+    if m==1 :
+        for i in char:
+            n=alphabet.find(i)
+            n_shift=n+shift
+            if n_shift>=25 :
+                n_shift=n_shift-26
+            #print(n_shift)
+            outchar=alphabet[n_shift]
+            empty.append(outchar)
+            out="".join([str(j) for j in empty])
+        print(out)
+            
+    elif m==2:
+        for i in char:
+        
+            n=alphabet.find(i)
+            n_shift=n-shift
+            if n_shift>=25 :
+                n_shift=n_shift-26
+            #print(n_shift)
+            outchar=alphabet[n_shift]
+            empty.append(outchar)
+            out="".join([str(j) for j in empty])
+        print(out)
+        
+        
+        
     else:
-
-
-        n=alphabet.find(i)
-    n_shift=n+shift
-    if n_shift>=25 :
-        n_shift=n_shift-26
-    #print(n_shift)
-    outchar=alphabet[n_shift]
-    empty.append(outchar)
-    out="".join([str(j) for j in empty])
-   
- 
-print(out)
+        print("Your options are invalid :)")
